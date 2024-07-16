979a1f969009ead3051c88f6e6302be1cfff4a3c
aaliyandev3 <aaliyandev3@gmail.com>
2024-07-16 10:49:16 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
+import Base64 from 'base64-js';
+import MarkdownIt from 'markdown-it';
+import { maybeShowApiKeyBanner } from './gemini-api-banner';
+import './style.css';
+
+// 🔥🔥 
+let API_KEY = 'AIzaSyBQIj8EOOn0e5TCOWwTtrywrXdSz-dChvY';
+
+let form = document.querySelector('form');
+let promptInput = document.querySelector('input[name="prompt"]');
+let output = document.querySelector('.output');
+
+form.onsubmit = async (ev) => {
+  ev.preventDefault();
+  output.textContent = 'Thinking...';
+
+  try {
+    // Load the image as a base64 string
+    let imageUrl = form.elements.namedItem('chosen-image').value;
+    let imageBase64 = await fetch(imageUrl)
+      .then(r => r.arrayBuffer())
+      .then(a => Base64.fromByteArray(new Uint8Array(a)));
+
+    let contents = [
+      {
+        role: 'user',
+        parts: [
+          { inline_data: { mime_type: 'image/jpeg', data: imageBase64, } },
+          { text: promptInput.value }
+        ]
+      }
+    ];
+
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({
+      model: "gemini-1.5-pro", 
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+        },
+      ],
+    });
+
+    const result = await model.generateContentStream({ contents });
+
+    let buffer = [];
+    let md = new MarkdownIt();
+    for await (let response of result.stream) {
+      buffer.push(response.text());
+      output.innerHTML = md.render(buffer.join(''));
+    }
+  } catch (e) {
+    output.innerHTML += '<hr>' + e;
+  }
+};
+
